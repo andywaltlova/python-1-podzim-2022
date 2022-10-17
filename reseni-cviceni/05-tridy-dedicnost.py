6755be63b1800a5ece926b3a99b06bd6abf74879
awaltlov <awaltlov@redhat.com>
2022-10-17 21:39:51 +0200

    Add hw for lesson 5

--- reseni-cviceni/05-tridy-dedicnost.py
+++ reseni-cviceni/05-tridy-dedicnost.py
@@ -9,7 +9,22 @@
 # 2 Balík (zapni hlavu)
 # Pokračuj ve své práci pro zásilkovou společnost. Společnost nově doručuje i cenné balíky, které mají zadanou určitou hodnotu.
 
-# Vytvoř třídu CennyBalik, která dědí od třídy Balik. CennyBalik má navíc atribut hodnota, ostatní atributy dědí od třídy Balik.
+# Trida Balik z minuleho cviceni
+class Balik:
+    def __init__(self, adresa, hmotnost):
+        self.adresa = adresa
+        self.hmotnost = hmotnost
+        self.doruceno = False
+
+    def doruc(self):
+        self.doruceno = True
+
+    def __str__(self):
+        vypis = f'{self.adresa}({self.hmostnos} kg) - {self.doruceno}'
+        return vypis
+
+# Vytvoř třídu CennyBalik, která dědí od třídy Balik.
+# CennyBalik má navíc atribut hodnota, ostatní atributy dědí od třídy Balik.
 # Atribut hodnota nastav pomocí funkce __init__. Ostatní parametry předej funkci __init__ třídy Balik.
 # Vytvoř si alespoň jeden objekt a zkus volání jeho funkcí.
 
