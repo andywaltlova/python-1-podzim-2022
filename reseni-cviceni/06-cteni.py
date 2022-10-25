792533018ef03b59f395907130a0c74e7c8f88e6
awaltlov <awaltlov@redhat.com>
2022-10-25 21:18:42 +0200

    Solutions for week 6 exercises

--- reseni-cviceni/06-cteni.py
+++ reseni-cviceni/06-cteni.py
@@ -1,13 +1,32 @@
 # 1 Výplata přesněji (to dáš)
-# Zatím jsme výplatu počítali za předpokladu, že každý měsíc odpracujeme stejný počet hodin, což není příliš realistické.
-# Vytvořte proto textový soubor vykaz.txt, který bude obsahovat 12 řádků a na každém řádku počet odpracovaných hodin za každý měsíc za poslední rok.
+# Zatím jsme výplatu počítali za předpokladu,
+# že každý měsíc odpracujeme stejný počet hodin, což není příliš realistické.
+# Vytvořte proto textový soubor vykaz.txt, který bude obsahovat 12 řádků a na
+# každém řádku počet odpracovaných hodin za každý měsíc za poslední rok.
 
 # Otevřete tento soubor ve svém programu a načtěte hodnoty na řádcích do seznamu vykaz.
 # Vytiskněte tento seznam na konzoli funkcí print() abyste si ověřili, že jste soubor načetli správně.
 # Nechte uživatele zadat na příkazovém řádku hodinovou mzdu.
 # Spočítejte a na výstup vytiskněte celkovou výplatu za celý rok a průměrnou výplatu na jeden měsíc.
 
-# TODO
+with open('data/vykaz.txt', encoding='utf-8') as soubor:
+    hodiny = soubor.readlines()
+
+
+hodina_mzda = int(input('Zadej hodinovou mzdu: '))
+
+platy = [int(mesic) * hodina_mzda for mesic in hodiny]
+
+# platy = []
+# for mesic in hodiny:
+#     platy.append(int(mesic) * hodina_mzda)
+
+vydelek_rok = sum(platy)
+prumerny_mesicni_plat = vydelek_rok / 12
+
+print(vydelek_rok)
+print(prumerny_mesicni_plat)
+
 
 # 2 Počet slov (zapni hlavu)
 # Stáhněte si odevzdanou slohovou práci.
@@ -21,7 +40,17 @@
 # Vypište na výstup seznam hodnot udávající počty slov na každém řádku.
 # Vypište na výstup celkový počet všech slov v souboru.
 
-# TODO
+with open('data/praha.txt', encoding='utf-8') as soubor:
+    radky = soubor.readlines()
+
+print(radky)
+radky_slov = [radek.split() for radek in radky]
+print(radky_slov)
+radky_pocet_slov = [len(radek.split()) for radek in radky]
+print(radky_pocet_slov)
+
+print(sum(radky_pocet_slov))
+
 
 # Bonus
 
@@ -41,4 +70,23 @@
 # Upravte váš program tak, aby jméno souboru k otevření dostal na příkazové řádce,
 # abychom mohli takto zpracovávat výkazy z různých souborů, aniž bychom museli upravovat samotný kód programu.
 
-# TODO
+with open('data/auta.txt', encoding='utf-8') as soubor:
+    radky = soubor.readlines()
+
+modifikovane_radky = []
+for radek in radky:
+    if radek.strip() != '': # pokud radek obsahuje alespon jeden 'nebily' znak
+        hodnoty = radek.split(' ')
+        # spz, km = radek.split(' )
+        spz = hodnoty[0]
+        km = float(hodnoty[1].replace(',', '.'))
+        modifikovane_radky.append([spz,km])
+
+jen_km = [radek[1] for radek in modifikovane_radky]
+
+km_celkem = sum(jen_km)
+print(km_celkem)
+
+
+# osklivy one liner pomoci list comprehension
+print(f"{sum([float(radek.split()[1].replace(',','.')) for radek in radky if radek.strip() != ''])} tisic kilometru")
--- reseni-cviceni/06-zapis.py
+++ reseni-cviceni/06-zapis.py
@@ -5,16 +5,46 @@
 # Nejprve tyto informace vypište na výstup pomocí funkce print().
 # Poté program upravte tak, aby vypsal tyto výsledky do souboru.
 
-# TODO
+with open('data/vykaz.txt', 'r', encoding='utf-8') as vstup:
+    radky = [int(radek) for radek in vstup.readlines()]
+
+hodinova_mzda = 250
+platy = [hodinova_mzda*mesic for mesic in radky]
+
+# for mesic in radky:
+#     print(mesic * hodinova_mzda)
+
+print(platy)
+platy_k_zapisu = [str(plat) + '\n' for plat in platy]
+
+with open('data/mesicni_platy.txt', 'w', encoding='utf-8') as vystup:
+    # for plat in platy:
+    #     vystup.write(f'{plat}\n')
+        # vystup.write(str(plat) + '\n')
+    vystup.writelines(platy_k_zapisu)
+
 
 # 2 Hody kostkou (zapni hlavu)
 # Napište program, který vytvoří seznam deseti náhodných hodů dvanáctistěnnou kostkou.
 
 # Nejdříve tento seznam vytiskněte na konzoli pomocí funkce print().
 # Upravte váš program tak, aby náhodné hody kostkou vypsal do souboru s názvem hody.txt na jeden řádek oddělené čárkou a mezerou.
+
+# (NEDELALI JSME parametry prikazove radky https://kodim.cz/kurzy/python-data/zaklady-programovani/prvni-programy/moduly)
 # Upravte váš program tak, aby počet hodů dostal jako parametr na příkazové řádce. Zkuste použitím vašeho programu vyrobit 100, 1000 a 10 000 hodů.
 
-# TODO
+import random
+
+hody = [str(random.randint(1, 6)) for i in range(10)]
+print(hody)
+
+hody_zapis = ', '.join(hody) # musi byt retezce ne cisla
+
+with open('data/hody.txt', 'w', encoding='utf-8') as vystup:
+    # for hod in hody:
+    #     vystup.write(f'{hod},') # tady je trochu naprd ze i posledni cisl oza sebou bude mit carku
+
+    vystup.write(hody_zapis)
 
 # Bonus
 
@@ -27,4 +57,14 @@ pocty_dni = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 # Upravte váš program tak, aby zároveň s počtem dnů vypisoval i název měsíce. Oddělte v souboru název měsíce a počet dnů pomocí tabulátoru.
 # Upravte váš program tak, aby jako první řádek výsledného souboru obsahoval nadpisy pro jednotlivé sloupce, tedy měsíc a počet dní.
 
-# TODO
+with open('data/kalendar.txt', 'w', encoding='utf-8') as vystup:
+    for dny in pocty_dni:
+        vystup.write(f'{dny}\n')
+
+# Mohly byste si klidne udelat i slovnik misto listu
+mesice = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov','Dec']
+
+with open('data/kalendar.txt', 'w', encoding='utf-8') as vystup:
+    vystup.write('Month\tDays\n') # nadpisy sloupcu
+    for i in range(len(mesice)):
+        vystup.write(f'{mesice[i]}\t{pocty_dni[i]}\n')
